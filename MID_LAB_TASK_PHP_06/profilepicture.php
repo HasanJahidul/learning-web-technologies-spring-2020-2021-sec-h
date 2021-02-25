dc4d9b9c603fd532d2991865cbb1dc9b692c1a0b
HasanJahidul <joyaiub1@gmail.com>
2021-02-26 00:16:55 +0600

    3. Profile Picture

---
+++ MID_LAB_TASK_PHP_06/profilepicture.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+</head>
+<body>
+    
+    <form action="profilepicture.php" method="post" style="width:50px">
+                    <br><br><br><br><br>
+                    <fieldset >
+                  
+                  <legend><b>PROFILE PICTURE</b></legend>
+                  
+                  <img src="user.png" alt="User" height="150px" width="150px"><br>
+                  <input type="file" name="image"><hr>
+                  <input type="submit" name="submit" value="Submit">
+                  
+                  
+              </fieldset>
+        
+        
+    </form>
+    
+</body>
+</html>
